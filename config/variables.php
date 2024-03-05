26e14fafc831c8b4213dafc3645712e84f24093d
Mohammed Al-khafajiy <m_alkhafagiy@hotmail.co.uk>
2024-03-05 12:18:35 +0000

    Working version

--- config/variables.php
+++ config/variables.php
@@ -1,21 +1,21 @@
 <?php
   // Variables
   return [
-    "creatorName" => "ThemeSelection",
-    "creatorUrl" => "https://themeselection.com",
-    "templateName" => "Sneat",
+    "creatorName" => "Postechy",
+    "creatorUrl" => "https://www.postechy.com/",
+    "templateName" => "Postechy",
     "templateSuffix" => "Bootstrap 5 HTML Laravel Free Admin Template",
     "templateVersion" => "1.1.0",
     "templateFree" => true,
     "templateDescription" => "Most Powerful & Comprehensive Bootstrap 5 HTML Admin Dashboard Template built for developers!",
     "templateKeyword" => "dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5, bootstrap 5 free, free admin template",
-    "licenseUrl" => "https://themeselection.com/license/",
+    "licenseUrl" => "",
     "livePreview" => "https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template-free/demo/",
     "productPage" => "https://themeselection.com/item/sneat-free-bootstrap-html-laravel-admin-template/",
     "productPagePro" => "https://themeselection.com/item/sneat-bootstrap-laravel-admin-template/",
-    "support" => "https://github.com/themeselection/sneat-html-laravel-admin-template-free/issues",
+    "support" => "",
     "moreThemes" => "https://themeselection.com/",
-    "documentation" => "https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation",
+    "documentation" => "",
     "repository" => "https://github.com/themeselection/sneat-html-laravel-admin-template-free",
     "gitRepo" => "https://github.com/themeselection/sneat-bootstrap-html-laravel-admin-template-free.git",
     "gitRepoAccess" => "https://tools.themeselection.com/github/github-access",
@@ -23,6 +23,6 @@
     "facebookUrl" => "https://www.facebook.com/ThemeSelections/",
     "twitterUrl" => "https://twitter.com/Theme_Selection",
     "githubUrl" => "https://github.com/themeselection",
-    "dribbbleUrl" => "https://dribbble.com/themeselection",
-    "instagramUrl" => "https://www.instagram.com/themeselection/"
+    "dribbbleUrl" => "",
+    "instagramUrl" => ""
   ];
--- resources/views/layouts/sections/navbar/navbar.blade.php
+++ resources/views/layouts/sections/navbar/navbar.blade.php
@@ -33,26 +33,26 @@ $navbarDetached = ($navbarDetached ?? '');
       @endif
 
       <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-        <!-- Search -->
+        {{-- <!-- Search -->
         <div class="navbar-nav align-items-center">
           <div class="nav-item d-flex align-items-center">
             <i class="bx bx-search fs-4 lh-0"></i>
             <input type="text" class="form-control border-0 shadow-none ps-1 ps-sm-2" placeholder="Search..." aria-label="Search...">
           </div>
-        </div>
+        </div> --}}
         <!-- /Search -->
         <ul class="navbar-nav flex-row align-items-center ms-auto">
 
-          <!-- Place this tag where you want the button to render. -->
+          {{-- <!-- Place this tag where you want the button to render. -->
           <li class="nav-item lh-1 me-3">
             <a class="github-button" href="https://github.com/themeselection/sneat-html-laravel-admin-template-free" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star themeselection/sneat-html-laravel-admin-template-free on GitHub">Star</a>
-          </li>
+          </li>  --}}
 
           <!-- User -->
           <li class="nav-item navbar-dropdown dropdown-user dropdown">
             <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
               <div class="avatar avatar-online">
-                <img src="{{ asset('assets/img/avatars/1.png') }}" alt class="w-px-40 h-auto rounded-circle">
+                <img src="{{ asset('assets/img/avatars/user.png') }}" alt class="w-px-40 h-auto rounded-circle">
               </div>
             </a>
             <ul class="dropdown-menu dropdown-menu-end">
@@ -61,7 +61,7 @@ $navbarDetached = ($navbarDetached ?? '');
                   <div class="d-flex">
                     <div class="flex-shrink-0 me-3">
                       <div class="avatar avatar-online">
-                        <img src="{{ asset('assets/img/avatars/1.png') }}" alt class="w-px-40 h-auto rounded-circle">
+                        <img src="{{ asset('assets/img/avatars/user.png') }}" alt class="w-px-40 h-auto rounded-circle">
                       </div>
                     </div>
                     <div class="flex-grow-1">
